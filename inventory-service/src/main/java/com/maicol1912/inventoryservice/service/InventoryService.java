5636df1ebe72bc0992d390d12d0cad67bd9a86af
maicol1912 <maicolarcila34@gmail.com>
2023-05-11 13:30:48 -0500

    finish distributed tracing with zpikin ready to implement event driven with kafka

--- inventory-service/src/main/java/com/maicol1912/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/maicol1912/inventoryservice/service/InventoryService.java
@@ -5,6 +5,7 @@ import com.maicol1912.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +17,6 @@ import java.util.List;
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
-
     //* el metodo sera transaccional, es decir se ejecuta todo lo declarado o nada
 
     @Transactional(readOnly = true)
@@ -26,11 +26,9 @@ public class InventoryService {
         //* estamos recorriendo el metodo que devuelve un objeto completo de inventory
         //* recorremos cada posicion, y lo convertimos en el id, y si existe o no
         //* retornamos lista de objetos con id y boolean
-        log.info("Wait Started");
+        log.info("Cheking inventory");
         //* aca definimos que duerma por 10 segundos
-        Thread.sleep(10000);
-
-        log.info("Wait Ended");
+        //*Thread.sleep(10000);
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder().skuCode(inventory.getSkuCode())
--- order-service/src/main/java/com/maicol1912/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/maicol1912/orderservice/service/OrderService.java
@@ -9,6 +9,8 @@ import com.maicol1912.orderservice.model.OrderLineItems;
 import com.maicol1912.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -28,7 +30,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     //* debemos usar la de la clase, y esta tomara la implementacion que ya hizimos en el webClitnConfig
     private final WebClient.Builder webClientBuilder;
-
+    private final Tracer tracer;
     //* se recibe una lista de Ordenes
     public String placeOrder(OrderRequest orderRequest){
         //* instanciamos un objeto de tipo ORDER
@@ -50,29 +52,39 @@ public class OrderService {
                                       //* esto con cada iteracion,crea un objeto y le extrae el SkuCode
                                      .map(OrderLineItems::getSkuCode)
                                      .toList();
+        log.info("Calling inventory service");
 
-        //llama al servicio de inventory-service y mira si hay stock
-        //* lo cambiamos por el build debido a las multiples instancias, de un microservicio, para poder hacerlo
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                 .uri("http://inventory-service/api/inventory",
-                         //* definimos un uri builder y enviamos un param con nombre SkuCode y enviamos una lista
-                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build()
-                 )
-                 .retrieve()
-                 //* aca se coloca el tipo de la respuesta en este caso un array de la clase inventoy
-                 .bodyToMono(InventoryResponse[].class)
-                 //* para definir una peticion sincrona
-                 //* osea la ejecucion del codigo se detiene hastsa que este tenga una respuesta o tenga una excepcion
-                 .block();
-        //* el allMacth es para ver si todos cumplen con la misma condicion en este caso que todos esten en stock
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+        //* definimos un span con un nombre que queramos que salga en los logs de zipkin
+        Span inventoryServiceLookup = tracer.nextSpan().name("Inventory-ServiceLookup");
 
-        if(!allProductsInStock){
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        //* hacemos un try, definidiendo el start y el SpaInScope donde saldra, el nombre del service LooKup en el log
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+            //llama al servicio de inventory-service y mira si hay stock
+            //* lo cambiamos por el build debido a las multiples instancias, de un microservicio, para poder hacerlo
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            //* definimos un uri builder y enviamos un param con nombre SkuCode y enviamos una lista
+                            uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build()
+                    )
+                    .retrieve()
+                    //* aca se coloca el tipo de la respuesta en este caso un array de la clase inventoy
+                    .bodyToMono(InventoryResponse[].class)
+                    //* para definir una peticion sincrona
+                    //* osea la ejecucion del codigo se detiene hastsa que este tenga una respuesta o tenga una excepcion
+                    .block();
+            //* el allMacth es para ver si todos cumplen con la misma condicion en este caso que todos esten en stock
+            boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
 
+            if(!allProductsInStock){
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+
+            }
+            orderRepository.save(order);
+            return "Order Placed Successfully";
+        }finally {
+            inventoryServiceLookup.end();
         }
-        orderRepository.save(order);
-        return "Order Placed Successfully";
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
